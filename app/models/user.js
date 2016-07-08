ffb8f7db2b33f624874cb907b1c608daae5007a0
Hack Reactor Students <accounts@hackreactor.com>
2016-07-07 22:12:18 -0700

    PASSED ALL TESTS!!!...but one... :'(

--- app/models/user.js
+++ app/models/user.js
@@ -8,22 +8,27 @@ var Promise = require('bluebird');
 //   initialize: function() {
 //     this.on('creating', this.hashPassword);
 //   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function() {
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
 // });
-var Users = mongoose.model('users', {
-  username: String,
+var userSchema = mongoose.Schema({
+  username: {type: String, index: { unique: true}},
   password: String
 });
+
+var Users = mongoose.model('users', userSchema);
+
+userSchema.pre('save', function(next) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+});
+
+Users.prototype.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+};
 
 module.exports = Users;
